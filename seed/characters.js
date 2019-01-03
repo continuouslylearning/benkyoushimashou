256faf90da9f0ed0ee3d6f9793b43c021cf69b29
Timothy <superfluous11@gmail.com>
2019-01-03 13:22:30 -0800

    creates hiragana seed file

---
+++ seed/characters.js
@@ -0,0 +1,227 @@
+module.exports = [
+  {
+    character: 'ん',
+    system: 'hiragana',
+    romaji: 'n'
+  },
+  {
+    character: 'わ',
+    system: 'hiragana',
+    romaji: 'wa'
+  },
+  {
+    character: 'を',
+    system: 'hiragana',
+    romaji: 'wo'
+  },
+  {
+    character: 'ら',
+    system: 'hiragana',
+    romaji: 'ra'
+  },
+  {
+    character: 'り',
+    system: 'hiragana',
+    romaji: 'ri'
+  },
+  {
+    character: 'る',
+    system: 'hiragana',
+    romaji: 'ru'
+  },
+  {
+    character: 'れ',
+    system: 'hiragana',
+    romaji: 're'
+  },
+  {
+    character: 'ろ',
+    system: 'hiragana',
+    romaji: 'ro'
+  },
+  {
+    character: 'や',
+    system: 'hiragana',
+    romaji: 'ya'
+  },
+  {
+    character: 'よ',
+    system: 'hiragana',
+    romaji: 'yo'
+  },
+  {
+    character: 'ま',
+    system: 'hiragana',
+    romaji: 'ma'
+  },
+  {
+    character: 'み',
+    system: 'hiragana',
+    romaji: 'mi'
+  },
+  {
+    character: 'む',
+    system: 'hiragana',
+    romaji: 'mu'
+  },
+  {
+    character: 'め',
+    system: 'hiragana',
+    romaji: 'me'
+  },
+  {
+    character: 'も',
+    system: 'hiragana',
+    romaji: 'mo'
+  },
+  {
+    character: 'は',
+    system: 'hiragana',
+    romaji: 'ha'
+  },
+  {
+    character: 'ひ',
+    system: 'hiragana',
+    romaji: 'hi'
+  },
+  {
+    character: 'ふ',
+    system: 'hiragana',
+    romaji: 'fu'
+  },
+  {
+    character: 'へ',
+    system: 'hiragana',
+    romaji: 'he'
+  },
+  {
+    character: 'ほ',
+    system: 'hiragana',
+    romaji: 'ho'
+  },
+  {
+    character: 'な',
+    system: 'hiragana',
+    romaji: 'na'
+  },
+  {
+    character: 'に',
+    system: 'hiragana',
+    romaji: 'ni'
+  },
+  {
+    character: 'ぬ',
+    system: 'hiragana',
+    romaji: 'nu'
+  },
+  {
+    character: 'ね',
+    system: 'hiragana',
+    romaji: 'ne'
+  },
+  {
+    character: 'の',
+    system: 'hiragana',
+    romaji: 'no'
+  },
+  {
+    character: 'た',
+    system: 'hiragana',
+    romaji: 'ta'
+  },
+  {
+    character: 'ち',
+    system: 'hiragana',
+    romaji: 'chi'
+  },
+  {
+    character: 'つ',
+    system: 'hiragana',
+    romaji: 'tsu'
+  },
+  {
+    character: 'て',
+    system: 'hiragana',
+    romaji: 'te'
+  },
+  {
+    character: 'と',
+    system: 'hiragana',
+    romaji: 'to'
+  },
+  {
+    character: 'さ',
+    system: 'hiragana',
+    romaji: 'sa'
+  },
+  {
+    character: 'し',
+    system: 'hiragana',
+    romaji: 'shi'
+  },
+  {
+    character: 'す',
+    system: 'hiragana',
+    romaji: 'su'
+  },
+  {
+    character: 'せ',
+    system: 'hiragana',
+    romaji: 'se'
+  },
+  {
+    character: 'そ',
+    system: 'hiragana',
+    romaji: 'so'
+  },
+  {
+    character: 'か',
+    system: 'hiragana',
+    romaji: 'ka'
+  },
+  {
+    character: 'き',
+    system: 'hiragana',
+    romaji: 'ki'
+  },
+  {
+    character: 'く',
+    system: 'hiragana',
+    romaji: 'ku'
+  },
+  {
+    character: 'け',
+    system: 'hiragana',
+    romaji: 'ke'
+  },
+  {
+    character: 'こ',
+    system: 'hiragana',
+    romaji: 'ko'
+  },
+  {
+    character: 'あ',
+    system: 'hiragana',
+    romaji: 'a'
+  },
+  {
+    character: 'い',
+    system: 'hiragana',
+    romaji: 'i'
+  },
+  {
+    character: 'う',
+    system: 'hiragana',
+    romaji: 'u'
+  },
+  {
+    character: 'え',
+    system: 'hiragana',
+    romaji: 'e'
+  },
+  {
+    character: 'お',
+    system: 'hiragana',
+    romaji: 'o'
+  },
+];
